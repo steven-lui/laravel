55de694aeb8bcbffe52cbc245786565c5fb67dba
steven-lui <steven.lui@netmatters-scs.com>
2023-03-07 17:48:03 +0000

    Update 2023_03_02_140734_create_employees_table.php
    Refactor function calls

--- database/migrations/2023_03_02_140734_create_employees_table.php
+++ database/migrations/2023_03_02_140734_create_employees_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->foreignId('company_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('company_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone');
