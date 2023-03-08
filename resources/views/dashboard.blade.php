1997bfb7fd584041fd7c7579132e012512c9aa0c
steven-lui <steven.lui@netmatters-scs.com>
2023-03-08 10:29:10 +0000

    Update dashboard.blade.php
    Added helpful checklist

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,46 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+                    <p>Your project should include, but is not limited to:</p>
+                    <ul class="list-disc ml-4">
+                        <li class="text-green-400">
+                            Basic Laravel Auth: ability to log in as administrator
+                        </li>
+                        <li class="text-green-400">
+                            A seeder to create the first user with email admin@admin.com and password "password"
+                        </li>
+                        <li class="text-red-400">
+                            CRUD functionality (Create / Read / Update / Delete) for two menu items: Companies and
+                            Employees.
+                        </li>
+                        <li class="text-green-400">
+                            Companies Database table consists of these fields: Name (required), email, logo (minimum
+                            100?100), website
+                        </li>
+                        <li class="text-green-400">
+                            Employees Database table consists of these fields: First name (required), last name
+                            (required), Company, email, phone number
+                        </li>
+                        <li class="text-green-400">
+                            Use database migrations to create those schemas above
+                        </li>
+                        <li class="text-red-400">
+                            Store companies logos in storage/app/public folder and make them accessible from public
+                        </li>
+                        <li class="text-red-400">
+                            Use basic Laravel resource controllers with default methods ? index, create, store etc.
+                        </li>
+                        <li class="text-red-400">
+                            Use Laravel?s validation function, using Request classes
+                        </li>
+                        <li class="text-green-400">
+                            Use Laravel?s pagination for showing Companies/Employees list, 10 entries per page
+                        </li>
+                        <li class="text-green-400">
+                            Use <span class="line-through">https://github.com/laravel/ui</span> <span class="bold">https://github.com/laravel/breeze</span> as default Bootstrap-based design theme, but remove
+                            ability to register
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
