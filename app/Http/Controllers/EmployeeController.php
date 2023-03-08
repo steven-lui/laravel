c055195052b1e2b2cd39b0236f6dd6cc30422a9a
steven-lui <steven.lui@netmatters-scs.com>
2023-03-08 10:53:28 +0000

    Update EmployeeController.php
    Remove redundant use

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class EmployeeController extends Controller
 {
