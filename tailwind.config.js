59586e3a378e7bdcc8d11079b8d9a3551e710990
steven-lui <steven.lui@netmatters-scs.com>
2023-03-08 10:28:59 +0000

    Update tailwind.config.js
    Include Tailwind JIT to prevent some loading bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        // Tailwind JIT for pagination
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
 
     theme: {
