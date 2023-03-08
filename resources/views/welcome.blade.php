1b4283d6c8fd4c6d708ac40ea7fcebcdd14932f7
steven-lui <steven.lui@netmatters-scs.com>
2023-03-08 11:53:44 +0000

    Update Login
    Fixed width of login form
    Remove register include

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Laravel Reflection | Welcome</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -27,14 +27,8 @@
             </div>
         @endif
 
-        <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <div class="flex justify-center">
-                {{-- TODO LOGO --}}
-            </div>
-
-            <div>
-                @include('auth.register')
-            </div>
+        <div class="w-full sm:max-w-md">
+            @include('auth.login')
         </div>
     </div>
 </body>
