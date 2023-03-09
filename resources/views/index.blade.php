35c370407a215821e83d9e01b42175ea83211e5a
steven-lui <steven.lui@netmatters-scs.com>
2023-03-09 19:16:48 +0000

    Update app.blade.php
    Added extra styling to template
    Refactored redundant code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,14 @@
 <x-app-layout>
-    <div class="p-6 text-gray-900 dark:text-gray-100">
-        <x-table.table :data="$data" :columns="$columns" />
-        <div class="mt-4">
-            {{ $data->links() }}
-        </div>
+    <x-slot name="header">
+        {{ $header }}
+    </x-slot>
+
+    <x-table.table :data="$data" :columns="$columns" />
+    <div class="mt-2">
+        {{ $data->links() }}
+    </div>
+
+    <div class="flex justify-end mt-4">
+        <x-secondary-button>Add New {{ $header }}</x-secondary-button>
     </div>
 </x-app-layout>
